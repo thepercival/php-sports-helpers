cf83ad6830ca4470e968c4464a4889e178170998
thepercival <coendunnink@gmail.com>
2020-08-11 12:58:08 +0200

    add sportconfighelper

---
+++ domain/Base.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SportsHelpers;
+
+class SportConfig
+{
+    /**
+     * @var int
+     */
+    protected $nrOfFields;
+    /**
+     * @var int
+     */
+    protected $nrOfGamePlaces;
+
+    public function __construct(int $nrOfFields, int $nrOfGamePlaces)
+    {
+        $this->nrOfFields = $nrOfFields;
+        $this->nrOfGamePlaces = $nrOfGamePlaces;
+    }
+
+    public function getNrOfFields(): int
+    {
+        return $this->nrOfFields;
+    }
+
+    public function getNrOfGamePlaces(): int
+    {
+        return $this->nrOfGamePlaces;
+    }
+}
