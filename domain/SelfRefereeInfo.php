8ccd6446316b6dd5a0d39061891a1a4591efb6ed
thepercival <coendunnink@gmail.com>
2023-05-30 17:13:36 +0200

    fix(selfreferee): add info

--- domain/SelfRefereeInfo.php
+++ domain/SelfRefereeInfo.php
@@ -7,8 +7,12 @@ class SelfRefereeInfo
     public int $nrIfSimSelfRefs = 0;
     public function __construct(public SelfReferee $selfReferee, int $nrIfSimSelfRefs)
     {
-        if ($selfReferee !== SelfReferee::Disabled && $nrIfSimSelfRefs > 0) {
-            $this->nrIfSimSelfRefs = $nrIfSimSelfRefs;
+        if ($selfReferee !== SelfReferee::Disabled) {
+            if( $nrIfSimSelfRefs > 0 ) {
+                $this->nrIfSimSelfRefs = $nrIfSimSelfRefs;
+            } else {
+                $this->nrIfSimSelfRefs = 1;
+            }
         }
     }
 }
